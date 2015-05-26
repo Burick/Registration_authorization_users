fdfae25b42af7a80bee79ccdc761620c3ab0957c
Burick <cyberburick@gmail.com>
2015-05-26 22:41:44 +0300

    добавлены комменты в конфиге

--- config.php
+++ config.php
@@ -1,30 +1,23 @@
 <?php
 session_start();
- /** 
- * 
- * Защита от брутфорса
- * $file = stat('/tmp/pswd.lock'); 
-if ($file[9]-time() <= 2) showcapcha(); 
---- 
-if (wrong_password) touch('/tmp/pswd.lock');
-
-
-
- */
+/**
+* Файл настроек
+*/
+// параметры для подключения к базе
 define('CHARSET', 'UTF8');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'user_base');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_DSN', 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';charset='.CHARSET.';');
-
+// COKIE по умолчанию
 define('COOKIE_NAME', 'mySCripCookie');
 define('COOKIE_EXPIRE', time()+24*60*60*2);
 define('COOKIE_PATH', '/');
 define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST'] );
 define('COOKIE_SECURE', FALSE );
 define('COOKIE_HTTPOLY', TRUE );
-
+// Папка для загрузки картинок
 define('IMAGES_PATH', 'upload/' );
 
 function __autoload ($class_name) {
